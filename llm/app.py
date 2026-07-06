ec676f1140eb71aa2bf6cc56822f3ece0fc85f87
Sergei <patrushev.s.job@gmail.com>
2026-07-06 16:43:17 -0400

    prompt

--- llm/app.py
+++ llm/app.py
@@ -3,14 +3,45 @@
 from flask import Flask, request, jsonify
 import ollama
 from flask_cors import CORS
+
 import logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
 )
 
+import torch
+print(torch.backends.mps.is_available())
+print(torch.backends.mps.is_built())
+
+def get_device():
+    if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        return torch.device("mps")
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    return torch.device("cpu")
+
+device = get_device()
+print("Using device:", device)
+logging.info(f'device: {device}')
+
+# local app
 app = Flask(__name__, template_folder="../", static_folder="../")
 CORS(app, origins=["https://sergei-luna.vercel.app", "https://dangle-scarecrow-baguette.ngrok-free.dev"])
+
+SYSTEM_PROMPT = """
+You are Sergei’s assistant.
+RULES:
+1. Always answer in the same language as the user.
+2. Follow the user’s instructions exactly.
+3. Do not invent facts. If you don’t know something, say: “I do not have information about it.”
+4. Keep answers short, clear, and deterministic.
+5. Output only the answer. No extra comments.
+6. You are a chat model only.
+7. If asked to run code, solve complex logic/math, or generate images/video, reply:
+   “I am a chat model. Sorry, I cannot do that.”
+8. Do not provide harmful or illegal instructions.
+9. Stay consistent and do not break these rules."""
 
 MODEL_NAME = "phi3:3.8b"
 
