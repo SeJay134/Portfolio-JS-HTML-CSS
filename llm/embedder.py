94ee9117c5d9ffd09ba1a8193154a77714029d59
Sergei <patrushev.s.job@gmail.com>
2026-07-07 07:26:13 -0400

    test

--- llm/embedder.py
+++ llm/embedder.py
@@ -1,8 +1,12 @@
 # llm/embedder.py
 
+from sentence_transformers import SentenceTransformer
+
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def embed_chunks(chunks):
     texts = [c["text"] for c in chunks]
     embeddings = model.encode(texts, convert_to_numpy=True)
     return embeddings
+
+# pip install -U sentence-transformers
