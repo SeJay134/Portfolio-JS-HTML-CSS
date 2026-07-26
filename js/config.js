a89af71898a478b234361fa0b194bd37688329b5
Sergei <patrushev.s.job@gmail.com>
2026-07-25 20:20:06 -0400

    test

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const API_URL =
     window.location.hostname === "localhost"
-        ? "http://localhost:5002/chat"
+        ? "http://localhost:5500/chat"
         : "https://dangle-scarecrow-baguette.ngrok-free.dev/chat";
